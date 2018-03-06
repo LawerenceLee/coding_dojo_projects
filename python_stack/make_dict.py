f4b3b73b2abf27fadea3ba2f198be19e6fc79217
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-06 13:25:58 -0800

    finished make_dict

---
+++ python_stack/make_dict.py
@@ -0,0 +1,12 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+
+def make_dict(lst1, lst2):
+    animal_dict = {}
+    for x in range(len(lst1)):
+        animal_dict[lst1[x]] = lst2[x]
+    print(animal_dict)
+
+
+make_dict(name, favorite_animal)
