b117d0fc62004ef534fb32c16297f74ecad5c6ee
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-08 11:37:31 -0800

    finished dojo survey

---
+++ python_stack/flask_fundamentals/dojo_survey/app.py
@@ -0,0 +1,21 @@
+from flask import (Flask, render_template, request)
+
+
+DEBUG = True
+PORT = 8000
+HOST = "0.0.0.0"
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    return render_template("result.html", form=request.form)
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host=HOST, port=PORT)
