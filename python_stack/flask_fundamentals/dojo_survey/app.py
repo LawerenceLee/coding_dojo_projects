9bc5c0fe574ad56d465205b1f6aa464bc3e7204e
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-08 12:23:15 -0800

    added go back button to result.html, removed GET from result route

--- python_stack/flask_fundamentals/dojo_survey/app.py
+++ python_stack/flask_fundamentals/dojo_survey/app.py
@@ -12,7 +12,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/result", methods=["GET", "POST"])
+@app.route("/result", methods=["POST"])
 def result():
     return render_template("result.html", form=request.form)
 
