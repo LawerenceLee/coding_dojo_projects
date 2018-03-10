9999b5eba2a2b98a3ef11da515dd27d2559a037b
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-09 16:18:06 -0800

    finished counter

---
+++ python_stack/flask_fundamentals/counter_proj/app.py
@@ -0,0 +1,33 @@
+from flask import (Flask, render_template, redirect, session)
+
+
+DEBUG = True
+PORT = 8000
+HOST = "0.0.0.0"
+app = Flask(__name__)
+app.secret_key = "sdfasdfasdfasdasasdahwteh0835p26190248*65987y87ttabglazuv"
+
+
+@app.route('/')
+def index():
+    if "counter" not in session:
+        session['counter'] = 0
+    else:
+        session['counter'] += 1
+    return render_template('index.html')
+
+
+@app.route('/add_two')
+def add_two():
+    session['counter'] += 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session['counter'] = -1
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host=HOST, port=PORT)
