4d783067489e15302684bc8ba42ed27049f7d506
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-11 16:37:45 -0700

    completed dojo_survey w/ validation

---
+++ python_stack/flask_fundamentals/dojo_survey_with_validation/app.py
@@ -0,0 +1,33 @@
+from flask import (Flask, flash, render_template, request)
+
+
+DEBUG = True
+PORT = 8000
+HOST = "0.0.0.0"
+app = Flask(__name__)
+app.secret_key = ";alsj;akvp8wfoa973878ph&*&P*Hyfi7h;jBO^T^&Y&T&^R&Y*&YO*R*&TI"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    error = False
+
+    if not request.form['name'] or not request.form['comment']:
+        flash("Your form contains empty fields")
+        error = True
+    if len(request.form['comment']) > 120:
+        flash("The comment field has exceeded the 120 character limit")
+        error = True
+
+    if error:
+        return render_template("index.html", )
+    return render_template("result.html", form=request.form)
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host=HOST, port=PORT)
