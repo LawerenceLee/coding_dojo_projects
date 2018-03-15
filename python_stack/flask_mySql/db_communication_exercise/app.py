a7e6f3e590e3aab8e782a8ee308014fef2ed3bfb
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-14 18:50:32 -0700

    finished db_commuication exercise

---
+++ python_stack/flask_mySql/db_communication_exercise/app.py
@@ -0,0 +1,65 @@
+from flask import (Flask, g, render_template, flash, redirect, url_for,
+                   request)
+from mysqlconnection import MySQLConnector
+
+
+DEBUG = True
+PORT = 8000
+HOST = "0.0.0.0"
+app = Flask(__name__)
+mysql = MySQLConnector(app, 'friendsdb')
+
+
+@app.route('/')
+def index():
+    friends = mysql.query_db("SELECT * FROM friends")
+    return render_template('index.html', all_friends=friends)
+
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    # Write query to select specific user by id. At every point where
+    # we want to insert data, we write ":" and variable name.
+    query = "SELECT * FROM friends WHERE id = :specific_id"
+    # Then define a dictionary with key that matches :variable_name in query.
+    data = {'specific_id': friend_id}
+    # Run query with inserted data.
+    friends = mysql.query_db(query, data)
+    # Friends should be a list with a single object,
+    # so we pass the value at [0] to our template under alias one_friend.
+    return render_template('index.html', one_friend=friends[0])
+
+
+@app.route('/friends', methods=['POST'])
+def create():
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation']
+           }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+
+@app.route('/update_friend/<friend_id>', methods=['POST'])
+def update(friend_id):
+    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation'],
+             'id': friend_id
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+
+@app.route('/remove_friend/<friend_id>')
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG, host=HOST, port=PORT)
