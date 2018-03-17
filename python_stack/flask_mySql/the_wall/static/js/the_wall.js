ec953ed28a6dfe1330ceea98135e4e9ca4d942f2
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-17 12:26:32 -0700

    fixed issue with comment textareas extending outside their container

--- python_stack/flask_mySql/the_wall/static/js/the_wall.js
+++ python_stack/flask_mySql/the_wall/static/js/the_wall.js
@@ -10,17 +10,19 @@ $(document).ready(function(){
             }).done(function(data){
                 var message = '<div class="message_wrapper"><h3 class="message_info">' + data.message_info
                 message += '</h3><p class="message">' + data.message + '</p></div>'
-                message += '<div id="commenting"><h3>Post a comment</h2>'
-                message += '<textarea name="comment" id="message" cols="100" rows="3"'
+                message += '<div class="commenting"><h3>Post a comment</h2>'
+                message += '<textarea name="comment" cols="100" rows="3"'
                 message += 'message-id="' + data.message_id +'"'
                 message += '></textarea><button>Post a comment</button></div>'
                 $("#messages_box").prepend(message)
             });
         }
         else if ($(this).text() == "Post a comment" && $(".commenting>textarea").val()) {
+            
             var comment_txt = $(".commenting>textarea").val()
             var message_identifier = $(".commenting>textarea").attr("message-id")
             $(".commenting>textarea").val("")
+
             $.ajax({
                 data : {comment_text : comment_txt, message_id : message_identifier},
                 type: "POST",
