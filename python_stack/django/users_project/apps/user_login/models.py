bce2102a11906b04a987cb56cbddeefeb1f340d1
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-20 15:19:54 -0700

    completed users project in django

---
+++ python_stack/django/users_project/apps/user_login/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.EmailField()
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
