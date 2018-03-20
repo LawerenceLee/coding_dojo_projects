305a38a4697a93928fa52c764f3f9cfae04f8b5d
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-20 16:18:11 -0700

    completed dojo ninjas

---
+++ python_stack/django/dojo_ninjas_project/apps/dojo_ninjas/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField()
+
+
+class Ninjas(models.Model):
+    dojo_id = models.ForeignKey(Dojos, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
