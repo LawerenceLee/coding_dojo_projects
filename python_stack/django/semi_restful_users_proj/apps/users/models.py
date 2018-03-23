75751033e81a098c4daa43f78c5d11cd069f9dce
Lawerence Lee <LawerenceLee@users.noreply.github.com>
2018-03-22 18:13:29 -0700

    Update models.py

--- python_stack/django/semi_restful_users_proj/apps/users/models.py
+++ python_stack/django/semi_restful_users_proj/apps/users/models.py
@@ -5,8 +5,9 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255) 
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    changed_at = models.DateTimeField(auto_now=True)
