1199b6dfe7a81557cabcf7c64ed8f31b079a629e
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-24 23:43:13 -0700

    added reviews_main and book_detail routes

--- python_stack/django/belt_reviewer_proj/book_reviews_app/views.py
+++ python_stack/django/belt_reviewer_proj/book_reviews_app/views.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from models import Author, Book, Review
 
 
 def reviews_main(request):
+    reviews = Review.objects.all().order_by("created_at")[:3]
+    books = Book.objects.all()
+    print(books)
     return render(
-        request, 'book_reviews_app/reviews_main.html', 
+        request, 'book_reviews_app/reviews_main.html',
+        {"books": books, "reviews": reviews},
     )
+
+
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        messages.error(request, "The book requested does not exist")
+        return redirect("/books")
+    return render(
+        request, "book_reviews_app/book_detail.html",
+        {"book": book}
+    )
+
+# delete view for reviews
+# if review.user.id == request.session.user_id then delete
