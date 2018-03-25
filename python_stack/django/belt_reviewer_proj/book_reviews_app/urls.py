bbb17433e365516c73906e2ece215b2dff3fc263
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-24 23:42:32 -0700

    added books/<int> url

--- python_stack/django/belt_reviewer_proj/book_reviews_app/urls.py
+++ python_stack/django/belt_reviewer_proj/book_reviews_app/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^books$', views.reviews_main)
+    url(r'^books$', views.reviews_main),
+    url(r'^books/(?P<id>\d+)$', views.book_detail),
+    # url(r'^users/(?P<id>\d+)$', views.user_profile),
 ]
