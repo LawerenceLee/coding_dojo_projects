60289658bf685fea58b734fb10cace7507f96a52
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-25 15:49:54 -0700

    added null=True to user_alias

--- python_stack/django/belt_reviewer_proj/log_reg_app/models.py
+++ python_stack/django/belt_reviewer_proj/log_reg_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(models.Model):
-    alias = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255, null=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
